c54033f5146448090d46c54d59ff0176c24c249a
Iaroslav Kriuchkov <iaroslav.kriuchkov@gmail.com>
2023-01-21 01:45:51 +0200

    Revert "Initial commit. Release of v0.0.1"
    This reverts commit fec8ed78388c7f798b52c3f3e126d03f341a16b0.

--- src/roadtraffic/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import dependencies
-from . import utils
-from . import fintraffic
-
-# Define __all__
-__all__ = ["utils", "fintraffic"]
--- src/roadtraffic/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# Import dependencies
-from . import constant
-from . import load
-from . import funcs
-from . import process
-
-# Define __all__
-__all__ = ["constant", "load", "funcs", "process"]
